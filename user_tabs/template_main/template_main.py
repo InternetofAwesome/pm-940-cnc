943dacddba02af9b81b64d6565fa01f576cb9d3e
Sam Anderson <me@samsurl.colm>
2026-02-21 01:10:15 -0800

    update tuning parameters. Still not perfect, but workable. Tools added/changed, and some wip on custom screens

--- user_tabs/template_main/template_main.py
+++ user_tabs/template_main/template_main.py
@@ -21,22 +21,3 @@ class UserTab(QWidget):
         super(UserTab, self).__init__(parent)
         ui_file = os.path.splitext(os.path.basename(__file__))[0] + ".ui"
         uic.loadUi(os.path.join(os.path.dirname(__file__), ui_file), self)
-
-        # Connect the Spindle Nose Touch Off button
-        if hasattr(self, 'btnSpindleNoseTouchOff'):
-            self.btnSpindleNoseTouchOff.clicked.connect(self.spindle_nose_touch_off)
-
-    def spindle_touch_off(self):
-        """
-        Run the spindle nose touch off subroutine via MDI command.
-        """
-        try:
-            c = linuxcnc.command()
-            # Make sure machine is on and in MDI mode
-            # Run the subroutine (assuming o<tool_touch_off> is loaded and available)
-            c.mode(linuxcnc.MODE_MDI)
-            c.wait_complete()
-            c.mdi("o<spindle_nose_touch_off> call")
-            LOG.info("Spindle Nose Touch Off subroutine called.")
-        except Exception as e:
-            LOG.error(f"Failed to run spindle nose touch off: {e}")
